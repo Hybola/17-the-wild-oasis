87cb9d98b7a738826ef8b4f60a83cd85a07fd88c
Hybola <keeratipong.b@gmail.com>
2024-08-25 20:20:50 +0700

    lecture 354: handle error submit form

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -8,9 +8,11 @@ const StyledAppLayout = styled.div`
   grid-template-columns: 26rem 1fr;
   grid-template-rows: auto 1fr;
 `;
+
 const Main = styled.main`
   background-color: var(--color-grey-50);
   padding: 4rem 4.8rem 6.4rem;
+  overflow: scroll;
 `;
 const Container = styled.div`
   max-width: 120rem;
