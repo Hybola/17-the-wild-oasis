924f626f6d3a140e9d1fbe112a586b1b412fa1fb
Hybola <keeratipong.b@gmail.com>
2024-08-27 17:28:51 +0700

    lecture 359

--- src/features/cabins/CreateCabinForm-v1.jsx
+++ src/features/cabins/CreateCabinForm-v1.jsx
@@ -6,7 +6,7 @@ import Form from "../../ui/Form";
 import Button from "../../ui/Button";
 import FileInput from "../../ui/FileInput";
 import Textarea from "../../ui/Textarea";
-import FormRow from "./FormRow";
+import FormRow from "../../ui/FormRow";
 
 import { useForm } from "react-hook-form";
 import { createCabin } from "../../services/apiCabins";
--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -5,7 +5,7 @@ import Form from "../../ui/Form";
 import Button from "../../ui/Button";
 import FileInput from "../../ui/FileInput";
 import Textarea from "../../ui/Textarea";
-import FormRow from "./FormRow";
+import FormRow from "../../ui/FormRow";
 
 import { useCreateCabin } from "./useCreateCabin";
 import { useEditCabin } from "./useEditCabin";
