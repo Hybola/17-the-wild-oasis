e20f44836468c2279917e6b6faf59d2fa31cedf5
Hybola <keeratipong.b@gmail.com>
2024-09-01 15:33:00 +0700

    lecture 372

--- src/ui/Table.jsx
+++ src/ui/Table.jsx
@@ -84,8 +84,9 @@ function Row({ children }) {
   );
 }
 
-function Body() {
-  return;
+function Body({ data, render }) {
+  if (!data.length) return <Empty>No data to show at the moment</Empty>;
+  return <StyledBody>{data.map(render)}</StyledBody>;
 }
 
 Table.Header = Header;
