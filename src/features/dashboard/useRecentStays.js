93b11154535458f2b674148007ec9ae62205e468
Hybola <keeratipong.b@gmail.com>
2024-09-23 14:52:26 +0700

    lecture 403: PieChart

--- src/features/dashboard/useRecentStays.js
+++ src/features/dashboard/useRecentStays.js
@@ -13,7 +13,7 @@ export function useRecentStays() {
     queryKey: ["stays", `last-${numDays}`],
   });
   const confirmedStays = stays?.filter(
-    (stay) => stay.status === "checked-in" || stays.status === "checked-out"
+    (stay) => stay.status === "checked-in" || stay.status === "checked-out"
   );
 
   return { stays, confirmedStays, isLoading, numDays };
